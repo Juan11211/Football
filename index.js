7f0efe84f61e7bc8982133ad0f5c78b78efe567c
Juan11211 <avilesjuan628@gmail.com>
2023-11-02 10:27:23 -0400

    dropdown menu working

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-"use strict"
+"use strict";
 
-window.onload = init;
+window.onload = function () {
+    initStateDrops();
+    const selectTeam = document.getElementById("selectTeam");
+    selectTeam.onchange = submitTeamBtnClicked;
+};
 
-
-
-function initStateDrops(){
-
-    // load 
+function initStateDrops() {
+    // load teams
     let teams = [
-        {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
-        {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
-        {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
-        {code:"KAN", name:"Kansas City Chiefs",
-        plays:"Kansas City, MO"},
-        ];
+        { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
+        { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
+        { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
+        { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
+    ];
 
     const selectTeam = document.getElementById("selectTeam");
 
-
-    for(let i = 0; i < teams.length; i++){
-        
+    for (let i = 0; i < teams.length; i++) {
+        let option = new Option(teams[i].name, teams[i].code);
+        selectTeam.appendChild(option);
     }
-
-
-
-
-
-
-
-
-
 }
+
+function submitTeamBtnClicked() {
+    const selectTeam = document.getElementById("selectTeam");
+    const selectedTeamCode = selectTeam.value;
+
+    console.log(`Selected Team: ${selectedTeamCode}`);
+}
+
