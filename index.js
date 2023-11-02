60840660a2d511607ffa256d7f81b96e75fdfc62
Juan11211 <avilesjuan628@gmail.com>
2023-11-02 07:43:36 -0400

    working on football select

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict"
+
+window.onload = init;
+
+
+
+function initStateDrops(){
+
+    // load 
+    let teams = [
+        {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
+        {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
+        {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
+        {code:"KAN", name:"Kansas City Chiefs",
+        plays:"Kansas City, MO"},
+        ];
+
+    const selectTeam = document.getElementById("selectTeam");
+
+
+    for(let i = 0; i < teams.length; i++){
+        
+    }
+
+
+
+
+
+
+
+
+
+}
